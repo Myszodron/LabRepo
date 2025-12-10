c1b55c05773988ea0fb24ac91a846ce09fb7d653
Myszodron <darrak300@gmail.com>
2025-12-10 02:50:57 +0100

    determined the attributes that each class should contain

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 public class Main {
+    //Main doesn't contain any attributes
+    public static void main(String[] args) {
+    }
 }
 
 class Matrix {
+
+    //Attributes
+    private int rows;
+    private int columns;
+    private int[][] data;
 }
 
 class Circle {
+
+    //Attributes
+    private double radius;
 }
 
 class Sphere {
+
+    //Attributes
+    private double radius;
 }
+
 
