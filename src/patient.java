80f67b72bb4cc4b962c8622f2adabea10c7f4c6b
Myszodron <darrak300@gmail.com>
2025-12-17 10:27:23 +0100

    introduce + Treatable + condition

--- src/patient.java
+++ src/patient.java
@@ -12,4 +12,24 @@ abstract class Person {
         this.name = name;
         this.age = age;
     }
+
+    public void introduce() {
+        System.out.println("My name is " + name + " and I'm " + age + " years old.");
+    }
 }
+
+class Patient extends Person implements Treatable {
+
+    private String condition;
+
+    // Constructor initializes person data and patient condition
+    public Patient(String name, int age, String condition) {
+        super(name, age);
+        this.condition = condition;
+    }
+
+    public void receiveTreatment() {
+        System.out.println("Patient with condition \"" + condition + "\" is receiving treatment.");
+    }
+}
+
