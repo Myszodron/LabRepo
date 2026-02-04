018f2c6eb15a70e3c2d880d871d824e3edcb7099
Myszodron <darrak300@gmail.com>
2026-02-04 10:28:20 +0100

    lab12

---
+++ src/lab12/HeapSort.java
@@ -0,0 +1,58 @@
+package lab12;
+
+/**
+ * Encapsulates the heap sort algorithm.
+ *
+ * The class owns the array it sorts and uses a MaxHeap
+ * to perform the sorting operation.
+ *
+ * Heap Sort has a time complexity of O(n log n) in all cases
+ * and is not a stable sorting algorithm.
+ */
+public class HeapSort {
+
+    private final int[] data;
+
+    /**
+     * Creates a HeapSort object for the given array.
+     *
+     * @param array array to be sorted
+     */
+    public HeapSort(int[] array) {
+        this.data = array;
+    }
+
+    /**
+     * Sorts the internal array in ascending order.
+     */
+    public void sort() {
+        MaxHeap heap = new MaxHeap(data);
+
+        for (int i = data.length - 1; i >= 0; i--) {
+            data[i] = heap.extractMax();
+        }
+    }
+
+    /**
+     * Checks whether the array is sorted correctly.
+     *
+     * @return true if sorted, false otherwise
+     */
+    public boolean isSorted() {
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] > data[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns the sorted array.
+     *
+     * @return sorted array
+     */
+    public int[] getArray() {
+        return data;
+    }
+}
